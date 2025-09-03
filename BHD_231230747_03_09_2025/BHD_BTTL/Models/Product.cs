7201406902c23735d404ea1694fcd18989441f77
DuckcyzZz <ducbanca1604@gmail.com>
2025-09-03 19:53:49 +0700

    add(file): View

---
+++ BHD_231230747_03_09_2025/BHD_BTTL/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿namespace BHD_BTTL.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+
+        public List<Product> GetProductList()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "GoldSun GRC5000 Electric Rice Cooker",
+                    Image = "/images/products/noi_com.webp",
+                },
+                new Product()
+                {
+                    Name = "GoldSun GRC5000 Electric Rice Cooker",
+                    Image = "/images/products/noi_com.webp",
+                },
+                new Product()
+                {
+                    Name = "GoldSun GRC5000 Electric Rice Cooker",
+                    Image = "/images/products/noi_com.webp",
+                },
+            };
+            return products;
+        }
+    }
+}
